3dcf26b15a44840ab7a73edd1b0ff19ea3142195
Anh Tran Hoang <anhthkt@gmail.com>
2024-01-16 15:53:43 +0700

    2024-1-16

--- dhis2-tools/src/components/importOrg/importOrg.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ImportOrg = () => {
-  return <div>This is Tool 1 content</div>;
-};
-
-export default ImportOrg;
--- dhis2-tools/src/components/tools2/tools.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Tool2 = () => {
-  return <div>This is Tool 2 content</div>;
-};
-
-export default Tool2;
