2cfb13bcdc8a7e9479893a0c4855af7817d6ed88
anhth.laptop <anhth.cntt@gmail.com>
2024-08-08 06:37:35 +0700

    20240808

--- dhis2/hat_reports/java-scripts/convert Tei Json to Excel import.js
+++ dhis2/hat_reports/java-scripts/convert Tei Json to Excel import.js
@@ -1,33 +1,60 @@
 const fs = require('fs');
 const XLSX = require('xlsx');
+const axios = require('axios');
 
-// Đọc tệp JSON
-const data = fs.readFileSync('./trackedEntityInstances.json', 'utf8');
-const jsonData = JSON.parse(data);
+const authentication = {
+    username: 'anhth',
+    password: 'Csdl2018@)!*'
+};
 
-// Chuyển đổi JSON thành định dạng Excel
-const workbook = XLSX.utils.book_new();
-const worksheetData = [['Họ tên', 'Giới tính', 'Ngày sinh', 'Mã BHYT', 'Xã']];
+// Hàm để chuyển đổi ngày từ yyyy-mm-dd sang dd/mm/yyyy
+const formatDate = (dateString) => {
+    if (!dateString) return '';
+    const [year, month, day] = dateString.split('-');
+    return `${day}/${month}/${year}`;
+};
 
-jsonData.trackedEntityInstances.forEach(instance => {
-    const attributes = instance.attributes.reduce((acc, attr) => {
-        acc[attr.attribute] = attr.value;
-        return acc;
-    }, {});
+async function convertTei() {
+    const url = 'https://kln.tkyt.vn/api/trackedEntityInstances.json?ou=nmijk75Nfap&ouMode=ACCESSIBLE&program=NAleauPZvIE&attribute=Gy1fkmBZpFk:EQ:Tcc7DwL9Gyy&fields=programOwners,attributes&paging=false';
+    
+    try {
+        const response = await axios.get(url, { auth: authentication });
+        const jsonData = response.data;
 
-    const rowData = [
-        attributes['xBoLC0aruyJ'] || '',
-        attributes['rwreLO34Xg7'] || '',
-        attributes['C7USC9MC8yH'] || '',
-        attributes['JHb1hzseNMg'] || '',
-        instance.programOwners[0].ownerOrgUnit || ''
-    ];
+        // Chuyển đổi JSON thành định dạng Excel
+        const workbook = XLSX.utils.book_new();
+        const worksheetData = [['Họ tên', 'Giới tính', 'Ngày sinh', 'Mã BHYT', 'Xã']];
 
-    worksheetData.push(rowData);
-});
+        jsonData.trackedEntityInstances.forEach(instance => {
+            const attributes = instance.attributes.reduce((acc, attr) => {
+                acc[attr.attribute] = attr.value;
+                return acc;
+            }, {});
 
-const worksheet = XLSX.utils.aoa_to_sheet(worksheetData);
-XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+            const formattedDateOfBirth = formatDate(attributes['C7USC9MC8yH']);
 
-// Lưu tệp Excel
-XLSX.writeFile(workbook, 'outputTei.xlsx');
+            const rowData = [
+                attributes['xBoLC0aruyJ'] || '',
+                attributes['rwreLO34Xg7'] || '',
+                formattedDateOfBirth,
+                attributes['JHb1hzseNMg'] || '',
+                instance.programOwners[0].ownerOrgUnit || ''
+            ];
+
+            worksheetData.push(rowData);
+        });
+
+        const worksheet = XLSX.utils.aoa_to_sheet(worksheetData);
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+        // Lưu tệp Excel
+        XLSX.writeFile(workbook, 'outputTei.xlsx');
+        
+        console.log('Tệp Excel đã được tạo thành công.');
+
+    } catch (error) {
+        console.error('Lỗi khi thực hiện yêu cầu hoặc xử lý dữ liệu:', error);
+    }
+}
+
+convertTei();
