1c0bc47cf1d19033f338251773120a56eed6fcfa
Anh Tran Hoang <anhthkt@gmail.com>
2024-08-09 16:32:00 +0700

    20240809

---
+++ dhis2/hat_reports/java-scripts/updateCodeBHYT.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const authentication = {
+    username: 'anhth',
+    password: 'Csdl2018@)!*'
+};
+
+async function main() {
+    const url1 = 'http://103.124.60.92/baocao'; 
+    const url2 = 'https://baocao.tkyt.vn'; 
+    const orgId = 'pajSJ8hWr1F'; // Đắk Nông
+
+    
+    let urlApi1 = `${url1}/api/organisationUnits.json?fields=:owner&paging=false&filter=path:ilike:${orgId}`;
+    let res1 = await axios.get(urlApi1, { auth: authentication });
+    let orgSrc = res1.data.organisationUnits;
+
+    
+    let urlApi2 = `${url2}/api/organisationUnits.json?fields=:owner&paging=false&filter=path:ilike:${orgId}`;
+    let res2 = await axios.get(urlApi2, { auth: authentication });
+    let orgDes = res2.data;
+
+    
+    const attributeId = 'Os56slbI9so';
+    orgSrc.forEach(srcUnit => {
+        let attributeValue = srcUnit.attributeValues.find(attr => attr.attribute.id === attributeId);
+        if (attributeValue) {
+            let destUnit = orgDes.organisationUnits.find(destUnit => destUnit.id === srcUnit.id);
+            if (destUnit) {
+                let destAttribute = destUnit.attributeValues.find(attr => attr.attribute.id === attributeId);
+                if (destAttribute) {
+                    
+                    destAttribute.value = attributeValue.value;
+                } else {
+           
+                    destUnit.attributeValues.push({
+                        value: attributeValue.value,
+                        attribute: {
+                            id: attributeId
+                        }
+                    });
+                }
+            }
+        }
+    });
+
+   
+    fs.writeFileSync('updatedOrgDes.json', JSON.stringify(orgDes, null, 2), 'utf8');
+
+    console.log('Lưu file thành công updatedOrgDes.json');
+}
+
+main().catch(error => console.error(error));
